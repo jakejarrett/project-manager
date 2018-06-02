81dc7af49f60a33a6c7a9dad964d58c1faaecb69
Jake <jakeryanjarrett@gmail.com>
2018-06-02 22:28:43 +1000

    Clean up code

--- src/components/app.js
+++ src/components/app.js
@@ -110,7 +110,7 @@ export default class App extends Component {
 	}
 
 	addProject () {
-		console.log(this);
+		//
 	}
 
 	render() {
--- src/components/assignee/assignee.js
+++ src/components/assignee/assignee.js
@@ -2,10 +2,8 @@ import style from './style';
 
 export const Assignee = ({ children, ...props }) => (
     <span class={style['reporter-info']}>
-    {   console.log(props.issue)}
         {/*
             Add in images & tooltip for image
-            Assignee component
         */}
         <div class={style['image-container']} style={`background-image: url(${props.issue.image})`}></div>
         {props.issue.assignee}
